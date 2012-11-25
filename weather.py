d4022ebc2bf1b68f000ad3d08762c121f9ac3ba8
Ryosuke HAYASAKA <hayasaka.ryosuke@gmail.com>
2012-11-25 14:33:58 +0900

    I removed unnecessary code

--- weather.py
+++ weather.py
@@ -3,8 +3,6 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
 import sys
-import codecs
-import commands
 import os
 URL="http://www.jma.go.jp/jp/yoho/306.html"
 
@@ -16,7 +14,7 @@ data= soup('th',{'class':'weather'})[0]
 weatherinfo="".encode('utf-8')
 weatherinfo=data('img')[0].get('title').encode('utf-8')
 result={}
-#if(0!=weatherinfo.find('曇')):
+
 result[unicode("☁ ",'utf-8')]=weatherinfo.find('曇')
 result[unicode("☀ ",'utf-8')]=weatherinfo.find('晴')
 result[unicode(" | ",'utf-8')]=weatherinfo.find('時々') 
@@ -24,16 +22,12 @@ result[unicode("->",'utf-8')]=weatherinfo.find('後')
 result[unicode("☃ ",'utf-8')]=weatherinfo.find('雪')
 result[unicode("☂ ",'utf-8')]=weatherinfo.find('雨')
 
-#print weatherinfo
-#print info('img')[0].get('title')
 
 f=codecs.open('weatherpy.cache','w','utf-8')
 output=""
 for data,num in sorted(result.items(),key=lambda x:x[1]):
     if(-1!=num):
         f.write(data)
-#print output
-#f.write(unicode(output,'utf-8'))
 f.close()
-os.system("cat ~/Documents/PythonWorks/weatherpy.cache")
+os.system("cat weatherpy.cache")
 
